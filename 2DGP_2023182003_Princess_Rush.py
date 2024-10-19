eebc16ceb3ccff716eb0a038fc52d407f6bbd5e9
Seojin.K <7701seojin@gmail.com>
2024-10-19 21:08:54 +0900

    창 끄기

--- 2DGP_2023182003_Princess_Rush.py
+++ 2DGP_2023182003_Princess_Rush.py
@@ -1,9 +1,47 @@
 from pico2d import*
 
+class Page:
+    def __init__(self):
+        pass
+    def update(self):
+        pass
+    def draw(self):
+        pass
 
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+
+def reset_world():
+    global running
+    global world
+    running = True
+    world = []
+    page = Page()
+    world.append(page)
+
+def update_world():
+    for o in world:
+        o.update()
+
+def render_world():
+    clear_canvas()
+    for o in world:
+        o.draw()
+    update_canvas()
 
 def main():
     open_canvas()
+    reset_world()
+    while running:
+        handle_events()
+        update_world()
+        render_world()
+        delay(0.05)
+    close_canvas()
 
 if __name__=='__main__':
     main()
