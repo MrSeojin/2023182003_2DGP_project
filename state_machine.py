e5e389d4682b59219523e6e42ea9e99e5931478a
Seojin.K <7701seojin@gmail.com>
2024-10-29 21:34:00 +0900

    state_machine 추가

---
+++ state_machine.py
@@ -0,0 +1,11 @@
+#이벤트 체크 함수 정의
+
+class StateMachine:
+    def __init__(self, obj):
+        self.obj = obj
+    def start(self):
+        pass
+    def update(self):
+        pass
+    def draw(self):
+        pass
