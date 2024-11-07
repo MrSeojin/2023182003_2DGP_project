ca8ff085f851c1494b3ff56b9ac73f2c8bc78fea
Seojin.K <7701seojin@gmail.com>
2024-11-08 03:01:13 +0900

    플레이 모드

---
+++ play_mode.py
@@ -0,0 +1,53 @@
+import game_framework
+from pico2d import *
+
+import game_world
+import title_mode
+from back_ground import Background
+from princess import Princess
+from mob import Mob
+
+def handle_events():
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            game_framework.change_mode(title_mode)
+        else:
+            princess.handle_event(event)
+
+def init():
+    global world
+    global princess
+    global mob
+    global background
+    running = True
+    world = []
+
+    background = Background()
+    game_world.add_object(background, 0)
+    princess = Princess()
+    game_world.add_object(princess, 1)
+    mob = Mob()
+    game_world.add_object(mob, 1)
+
+
+def finish():
+    game_world.clear()
+
+def update():
+    game_world.update()
+
+def draw():
+    clear_canvas()
+    game_world.render()
+    update_canvas()
+
+def pause():
+    pass
+
+def resume():
+    pass
