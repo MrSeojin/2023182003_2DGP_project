f58339aa91db449e5da280fa170ff4cd437b1236
Seojin.K <7701seojin@gmail.com>
2024-11-14 15:08:36 +0900

    floor time 조정

--- back_ground.py
+++ back_ground.py
@@ -1,22 +1,30 @@
 from pico2d import load_image
 
+import game_framework
+
+# princess Run Speed
+PIXEL_PER_METER = (100.0 / 20)  # 10 pixel 30 cm
+RUN_SPEED_KMPH = 20.0  # Km / Hour
+RUN_SPEED_MPM = (RUN_SPEED_KMPH * 1000.0 / 60.0)
+RUN_SPEED_MPS = (RUN_SPEED_MPM / 60.0)
+RUN_SPEED_PPS = (RUN_SPEED_MPS * PIXEL_PER_METER)
+
 class Background:
     def __init__(self):
-        self.image = load_image('map_background.png')
+        self.image = load_image('map_background_snow.png')
         self.count = 0
         self.x = 0
 
     def update(self):
-        if (self.count + 1) % 2 == 1 :
-            self.x += 1
+        self.x += RUN_SPEED_PPS * game_framework.frame_time
         if self.x >= 2000:
             self.x = 0
 
     def draw(self):
         if self.x > 800:
-            self.image.clip_draw(self.x, 0, 2000 - self.x, 600, (2000 - self.x) / 2, 300)
-            self.image.clip_draw(0, 0, self.x - 800, 600, 1200 - (self.x - 800) / 2, 300)
+            self.image.clip_draw(int(self.x), 0, 2000 - int(self.x), 600, (2000 - int(self.x)) / 2, 300)
+            self.image.clip_draw(0, 0, int(self.x) - 800, 600, 1200 - (int(self.x) - 800) / 2, 300)
         else:
-            self.image.clip_draw(self.x, 0, 1200, 600, 600, 300)
+            self.image.clip_draw(int(self.x), 0, 1200, 600, 600, 300)
 
 
